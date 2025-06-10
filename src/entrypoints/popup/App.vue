461fbcfba05c4daeaa45eec729ddd9c4cacb4240
nuttycc <73019368+nuttycc@users.noreply.github.com>
2025-06-10 13:18:32 +0800

    Add linting and formatting scripts, integrate Husky for pre-commit hooks, and update package dependencies. Remove old App.vue and add new entry points for popup functionality.

---
+++ src/entrypoints/popup/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+// import HelloWorld from '@/components/HelloWorld.vue';
+
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+</script>
+
+<template>
+  <div class="animate-zoom-in h-80 w-80">Hello World</div>
+</template>
+
+<style scoped></style>
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'wxt';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  srcDir: 'src',
   modules: ['@wxt-dev/module-vue'],
   vite: () => ({
-    plugins: [
-      tailwindcss(),
-    ],
-  })
+    plugins: [tailwindcss()],
+  }),
 });
